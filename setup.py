610097b0fe44d5b1907d99504e7358ad11a6441e
Nicholas Devenish <ndevenish@gmail.com>
2020-04-09 17:20:53 +0100

    more src layout

--- setup.py
+++ setup.py
@@ -4,10 +4,7 @@ from skbuild import setup
 setup(
     name="dials_accelerated_dispersion",
     packages=["dials_accelerated_dispersion"],
-    # setup_requires=setup_requirements,
-    test_suite="tests",
-    # tests_require=test_requirements,
-    url="https://github.com/DiamondLightSource/python-procrunner",
+    package_dir={'dials_accelerated_dispersion': 'src/dials_accelerated_dispersion'},
     version="0.1.0",
     zip_safe=False,
 )
