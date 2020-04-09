a1d918dd09bede841846876adc42086106a7e1d0
Nicholas Devenish <ndevenish@gmail.com>
2020-04-09 17:31:37 +0100

    more src layout and start adding entrypoint

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from skbuild import setup
 
-
 setup(
     name="dials_accelerated_dispersion",
     packages=["dials_accelerated_dispersion"],
-    package_dir={'dials_accelerated_dispersion': 'src/dials_accelerated_dispersion'},
+    package_dir={"dials_accelerated_dispersion": "src/dials_accelerated_dispersion"},
     version="0.1.0",
     zip_safe=False,
+    entry_points={
+        "dials.spotfinder.threshold": [
+            "accelerated_dispersion = dials_accelerated_dispersion:AcceleratedDispersionSpotfinderThreshold",
+        ],
+    },
 )
