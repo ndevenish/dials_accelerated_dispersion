d6d0bde502fcbf2e0b0a16fd8a0b42662da71f31
Nicholas Devenish <ndevenish@gmail.com>
2020-04-10 15:19:40 +0100

    Basic ext.cxx implementation

--- src/dials_accelerated_dispersion/__init__.py
+++ src/dials_accelerated_dispersion/__init__.py
@@ -1,3 +1,57 @@
 __all__ = ["AcceleratedDispersionSpotfinderThreshold"]
 
-from .dials_accelerated_dispersion_ext import AcceleratedDispersionSpotfinderThreshold
+from typing import TYPE_CHECKING
+
+from libtbx import Auto
+
+from .dials_accelerated_dispersion_ext import accelerated_dispersion
+
+if TYPE_CHECKING:
+    from libtbx.phil import scope_extract
+
+
+class AcceleratedDispersionSpotfinderThreshold:
+    name = "accelerated_dispersion"
+
+    def __init__(self, params: "scope_extract"):
+        self.params = params
+
+    def compute_threshold(self, image, mask):
+        """
+        Compute the threshold.
+
+        :param image: The image to process
+        :param mask: The pixel mask on the image
+        :returns: A boolean mask showing foreground/background pixels
+        """
+        # from dials.algorithms.spot_finding.threshold import DispersionThresholdStrategy
+
+        # self._algorithm = DispersionThresholdStrategy(
+        #     kernel_size=params.spotfinder.threshold.dispersion.kernel_size,
+        #     gain=params.spotfinder.threshold.dispersion.gain,
+        #     mask=params.spotfinder.lookup.mask,
+        #     n_sigma_b=params.spotfinder.threshold.dispersion.sigma_background,
+        #     n_sigma_s=params.spotfinder.threshold.dispersion.sigma_strong,
+        #     min_count=params.spotfinder.threshold.dispersion.min_local,
+        #     global_threshold=params.spotfinder.threshold.dispersion.global_threshold,
+        if self.params.spotfinder.threshold.dispersion.global_threshold is Auto:
+            threshold = 0
+        else:
+            threshold = int(
+                self.params.spotfinder.threshold.dispersion.global_threshold
+            )
+
+        output_data = None
+
+        accelerated_dispersion(
+            image=image,
+            mask=mask,
+            destination=output_data,
+            gain=self.params.spotfinder.threshold.dispersion.gain,
+            kernel_size=self.params.spotfinder.threshold.dispersion.kernel_size,
+            n_sigma_b=self.params.spotfinder.threshold.dispersion.sigma_background,
+            n_sigma_s=self.params.spotfinder.threshold.dispersion.sigma_strong,
+            threshold=threshold,
+            min_count=self.params.spotfinder.threshold.dispersion.min_local,
+        )
+        return output_data
--- src/dials_accelerated_dispersion/ext.cxx
+++ src/dials_accelerated_dispersion/ext.cxx
@@ -1,16 +1,67 @@
+#include <type_traits>
+#include <vector>
+
 #include <boost/python.hpp>
 
-class A {
-public:
-  int greet() {
-    return -424242;
-  }
-  bool eq(const A& right) {
-    return true;
-  }
-};
+#include <scitbx/array_family/accessors/c_grid.h>
+#include <scitbx/array_family/shared.h>
+#include <scitbx/array_family/tiny_types.h>
+
+#include "dispersion.h"
+
+namespace af = scitbx::af;
+
+void accelerated_dispersion(const af::const_ref<float, af::c_grid<2> > &src,
+                            const af::const_ref<bool, af::c_grid<2> > &mask,
+                            double gain,
+                            af::ref<bool, af::c_grid<2> > dst,
+                            af::int2 kernel_size,
+                            double nsig_b,
+                            double nsig_s,
+                            double threshold,
+                            int min_count) {
+    // Convert everything to plain C objects to pass through
+    const int width = src.accessor()[0];
+    const int height = src.accessor()[1];
+    const float *image = &src[0];
+
+    // Convert the mask to int for now
+    int *dst_int = new int[width * height];
+    std::vector<int> mask_int;
+    mask_int.reserve(width * height);
+    std::copy(mask.begin(), mask.end(), std::back_inserter(mask_int));
+
+    // Now call the function
+    ispc::dispersion_threshold(image,
+                               &mask_int[0],
+                               gain,
+                               dst_int,
+                               width,
+                               height,
+                               kernel_size[0],
+                               kernel_size[1],
+                               nsig_b,
+                               nsig_s,
+                               threshold,
+                               min_count,
+                               0);
+
+    // Copy the result int vector back
+    std::copy(dst_int, dst_int + width * height, dst.begin());
+    delete[] dst_int;
+}
 
 BOOST_PYTHON_MODULE(dials_accelerated_dispersion_ext) {
-  using namespace boost::python;
-  class_<A>("A").def("greet", &A::greet).def("__eq__", &A::eq);
+    using namespace boost::python;
+    def("accelerated_dispersion",
+        accelerated_dispersion,
+        args("image",
+             "mask",
+             "gain",
+             "destination",
+             "kernel_size",
+             "n_sigma_b",
+             "n_sigma_s",
+             "threshold",
+             "min_count"));
 }
